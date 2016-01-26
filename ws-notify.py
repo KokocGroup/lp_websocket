5a1a442536c396caa48b601027ce9e7504dd60ff
gotlium <gotlium@gmail.com>
2016-01-26 17:00:51 +0500

    new mem hosts

--- ws-notify.py
+++ ws-notify.py
@@ -37,14 +37,14 @@ REPLACE_RULE = (
 )
 
 parse_command_line()
-SESSION_REDIS_HOST = 'sessions.tst-1.dc-1.dlp3001.ru'
+SESSION_REDIS_HOST = 'sessions.mem.dlp3001.ru'
 if options.debug is True:
     SESSION_REDIS_HOST = 'localhost'
 SESSION_REDIS_PORT = 6379
 SESSION_REDIS_PASS = None
 SESSION_REDIS_DB = 4
 
-WS_REDIS_HOST = 'websocket.tst-1.dc-1.dlp3001.ru'
+WS_REDIS_HOST = 'websocket.mem.dlp3001.ru'
 if options.debug is True:
     WS_REDIS_HOST = 'localhost'
 WS_REDIS_PORT = 6379
